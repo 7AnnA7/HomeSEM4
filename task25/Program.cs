abcfc60cb993a97091bfd2e20ea04deb7e19bd44
AnnA <anna_elfimova@mail.ru>
2022-08-03 13:44:20 +0300

    task 25 A v stepeny B

---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int res = 1;
+for (int i = 1; i <= B; i++)
+{
+    res = res * A;
+}
+
+Console.WriteLine($"Число А в степени В: {res}");
