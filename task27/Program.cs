4a92391f6a30eda8e7e9ce5c88362205dd218dee
AnnA <anna_elfimova@mail.ru>
2022-08-03 15:35:21 +0300

    task 27 summa zifr v chisle

---
+++ task27/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int n = N; n > 0; sum = sum + n % 10, n = n / 10) ;
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
