da2481e1f4b049b38278619de414a84681e58703
AnnA <anna_elfimova@mail.ru>
2022-08-04 14:38:21 +0300

    task 29 massiv iz 8 elementov s konsoli na ekran

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (вводим массив с клавиатуры (т. е. через консоль :) ))
+int n = 8;
+int [] MyArr = new int [n];
+
+for (int i = 0; i < MyArr.Length; i++)
+{
+    Console.Write($"Введите один из восьми целочисленных элементов массива с индексом {i}: ");
+    MyArr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Массив из 8 элементов на экране: ");
+for (int i = 0; i < MyArr.Length; i++)
+{
+    Console.WriteLine(MyArr[i]);
+}
